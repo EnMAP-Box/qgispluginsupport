eb5365f325ffa8b4fea9100c279605d37bfc488b
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-01-22 12:49:53 +0100

    cleanup classification/__init__.py

--- qps/classification/__init__.py
+++ qps/classification/__init__.py
@@ -1,12 +1,1 @@
 
-
-import sys
-
-from qgis.core import QgsProject
-
-try:
-    from .classification_resources import qInitResources
-    qInitResources()
-except Exception as ex:
-    print('failed to initialize classification.classification_resources')
-
