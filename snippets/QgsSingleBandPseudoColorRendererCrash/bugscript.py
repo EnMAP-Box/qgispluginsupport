5479fa868afa876c9a12176b2ce7f85a07cb73c0
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-09-05 08:52:51 +0200

    added qgis crash description: see https://github.com/qgis/QGIS/issues/31568 for details
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

---
+++ snippets/QgsSingleBandPseudoColorRendererCrash/bugscript.py
@@ -0,0 +1,25 @@
+"""
+This script replicates a crash of the QGIS if run in the QGIS Python console
+"""
+
+from qgis.gui import *
+from qgis.core import *
+from qgis.utils import iface
+
+assert isinstance(iface, QgisInterface)
+
+layer = iface.mapCanvas().layers()[0]
+assert isinstance(layer, QgsRasterLayer)
+assert layer.bandCount() > 1
+renderer = layer.renderer()
+assert renderer.band() == 1
+assert isinstance(renderer, QgsSingleBandPseudoColorRenderer)
+
+# clone the render
+newRenderer = renderer.clone()
+newRenderer.setBand(2)
+layer.setRenderer(newRenderer)
+
+
+
+
