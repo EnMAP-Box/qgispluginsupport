1c33ac69db7b6445c1c507c3cfe3747b0fa5eb3d
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-10-02 09:09:30 +0200

    pipeline
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.bat
+++ runtests.bat
@@ -1,7 +1,7 @@
 :: use this script to run unit tests locally
 ::
 
-mkdir test-results
+mkdir test-reports
 set CI=True
 @echo off
 call :sub >test-results.txt
@@ -9,17 +9,17 @@ exit /b
 
 :sub
 python runfirst.py
-:: python -m nose2 --verbose discover tests "test_*.py"  > test-results/test_all.txt
-python -m nose2 -s tests test_spectrallibraries > test-results/test_spectrallibraries.txt
-python -m nose2 -s tests test_classificationscheme > test-results/test_classificationscheme.txt
-python -m nose2 -s tests test_crosshair > test-results/test_crosshair.txt
-python -m nose2 -s tests test_cursorlocationsvalues > test-results/test_cursorlocationsvalues.txt
-python -m nose2 -s tests test_init > test-results/test_init.txt
-python -m nose2 -s tests test_layerproperties  > test-results/test_layerproperties.txt
-python -m nose2 -s tests test_maptools > test-results/test_maptools.txt
-python -m nose2 -s tests test_models > test-results/test_models.txt
-python -m nose2 -s tests test_plotstyling > test-results/test_plotstyling.txt
-python -m nose2 -s tests test_qgisinstance > test-results/test_qgisinstance.txt
-python -m nose2 -s tests test_qgisissues > test-results/test_qgisissues.txt
-python -m nose2 -s tests test_testing > test-results/test_testing.txt
-python -m nose2 -s tests test_utils > test-results/test_utils.txt
+:: python -m nose2 --verbose discover tests "test_*.py"
+python -m nose2 -s tests test_spectrallibraries
+python -m nose2 -s tests test_classificationscheme
+python -m nose2 -s tests test_crosshair
+python -m nose2 -s tests test_cursorlocationsvalues
+python -m nose2 -s tests test_init
+python -m nose2 -s tests test_layerproperties
+python -m nose2 -s tests test_maptools
+python -m nose2 -s tests test_models
+python -m nose2 -s tests test_plotstyling
+python -m nose2 -s tests test_qgisinstance
+python -m nose2 -s tests test_qgisissues
+python -m nose2 -s tests test_testing
+python -m nose2 -s tests test_utils
--- tests/test_maptools.py
+++ tests/test_maptools.py
@@ -11,7 +11,7 @@
 __author__ = 'benjamin.jakimow@geo.hu-berlin.de'
 __copyright__ = 'Copyright 2019, Benjamin Jakimow'
 
-import unittest, pickle
+import unittest, xmlrunner
 from qps.testing import initQgisApplication, TestObjects
 QGIS_APP = initQgisApplication(loadProcessingFramework=False)
 from qgis import *
@@ -218,7 +218,7 @@ class TestMapTools(unittest.TestCase):
 
 if __name__ == "__main__":
     SHOW_GUI = False
-    unittest.main()
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-results'))
 
 
 
