8057a18ba766ccc670953d409e9beae2a1f3b113
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-10-02 11:39:18 +0200

    pipeline
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.bat
+++ runtests.bat
@@ -1,21 +1,20 @@
 
 :: use this script to run unit tests locally
 ::
-
-mkdir test-reports
 set CI=True
 python runfirst.py
 
-python -m nose2 -s tests test_classificationscheme & move nose2-junit.xml test-reports/test_classificationscheme.xml
-python -m nose2 -s tests test_crosshair & move nose2-junit.xml test-reports/test_crosshair.xml
-python -m nose2 -s tests test_cursorlocationsvalues & move nose2-junit.xml test-reports/test_cursorlocationsvalues.xml
-python -m nose2 -s tests test_init & move nose2-junit.xml test-reports/test_init.xml
-python -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/test_layerproperties.xml
-python -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/test_maptools.xml
-python -m nose2 -s tests test_models & move nose2-junit.xml test-reports/test_models.xml
-python -m nose2 -s tests test_plotstyling & move nose2-junit.xml test-reports/test_plotstyling.xml
-python -m nose2 -s tests test_qgisinstance & move nose2-junit.xml test-reports/test_qgisinstance.xml
-python -m nose2 -s tests test_qgisissues & move nose2-junit.xml test-reports/test_qgisissues.xml
-python -m nose2 -s tests test_spectrallibraries & move nose2-junit.xml test-reports/test_spectrallibraries.xml
-python -m nose2 -s tests test_testing & move nose2-junit.xml test-reports/test_testing.xml
-python -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/test_utils.xml
+mkdir test-reports\today
+python -m nose2 -s tests test_classificationscheme & move nose2-junit.xml test-reports/today/test_classificationscheme.xml
+python -m nose2 -s tests test_crosshair & move nose2-junit.xml test-reports/today/test_crosshair.xml
+python -m nose2 -s tests test_cursorlocationsvalues & move nose2-junit.xml test-reports/today/test_cursorlocationsvalues.xml
+python -m nose2 -s tests test_init & move nose2-junit.xml test-reports/today/test_init.xml
+python -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/today/test_layerproperties.xml
+python -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/today/test_maptools.xml
+python -m nose2 -s tests test_models & move nose2-junit.xml test-reports/today/test_models.xml
+python -m nose2 -s tests test_plotstyling & move nose2-junit.xml test-reports/today/test_plotstyling.xml
+python -m nose2 -s tests test_qgisinstance & move nose2-junit.xml test-reports/today/test_qgisinstance.xml
+python -m nose2 -s tests test_qgisissues & move nose2-junit.xml test-reports/today/test_qgisissues.xml
+python -m nose2 -s tests test_spectrallibraries & move nose2-junit.xml test-reports/today/test_spectrallibraries.xml
+python -m nose2 -s tests test_testing & move nose2-junit.xml test-reports/today/test_testing.xml
+python -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/today/test_utils.xml
--- snippets/create_runtestsbat.py
+++ snippets/create_runtestsbat.py
@@ -2,6 +2,8 @@ import os, sys, pathlib
 from qps.utils import file_search
 DIR_REPO = pathlib.Path(__file__).parents[1]
 DIR_TESTS = DIR_REPO / 'tests'
+
+
 PATH_RUNTESTS_BAT = DIR_REPO / 'runtests.bat'
 PATH_RUNTESTS_SH = DIR_REPO / 'runtests.sh'
 
@@ -11,22 +13,23 @@ PREFACE = \
 """
 :: use this script to run unit tests locally
 ::
-
-mkdir test-reports
 set CI=True
 python runfirst.py
 """
 
-
+dirOut = 'test-reports/today'
 linesBat = [PREFACE]
 linesSh = [PREFACE]
+linesBat.append('mkdir {}'.format(dirOut.replace('/', '\\')))
+linesSh.append('mkdir {}'.format(dirOut))
+
 
 for file in file_search(DIR_TESTS, 'test_*.py'):
 
     bn = os.path.basename(file)
     bn = os.path.splitext(bn)[0]
-    lineBat = 'python -m nose2 -s tests {0} & move {1} test-reports/{0}.xml'.format(bn, jUnitXML)
-    lineSh = 'python -m nose2 -s tests {0} ; mv {1} test-reports/{0}.xml'.format(bn, jUnitXML)
+    lineBat = 'python -m nose2 -s tests {0} & move {1} {2}/{0}.xml'.format(bn, jUnitXML, dirOut)
+    lineSh = 'python -m nose2 -s tests {0} ; mv {1} {2}/{0}.xml'.format(bn, jUnitXML, dirOut)
     linesBat.append(lineBat)
     linesSh.append(lineSh)
 
