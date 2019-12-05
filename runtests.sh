3aca07e07cf1447d244af5098ca388591b1f84d1
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2019-12-05 12:22:33 +0100

    CI tests
    Signed-off-by: benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.sh
+++ runtests.sh
@@ -14,7 +14,7 @@ python -m nose2 -s tests test_maptools ; mv nose2-junit.xml test-reports/today/t
 python -m nose2 -s tests test_models ; mv nose2-junit.xml test-reports/today/test_models.xml
 python -m nose2 -s tests test_plotstyling ; mv nose2-junit.xml test-reports/today/test_plotstyling.xml
 python -m nose2 -s tests test_qgisinstance ; mv nose2-junit.xml test-reports/today/test_qgisinstance.xml
-python -m nose2 -s tests test_qgisissues ; mv nose2-junit.xml test-reports/today/test_qgisissues.xml
+#python -m nose2 -s tests test_qgisissues ; mv nose2-junit.xml test-reports/today/test_qgisissues.xml
 python -m nose2 -s tests test_spectrallibraries ; mv nose2-junit.xml test-reports/today/test_spectrallibraries.xml
 python -m nose2 -s tests test_testing ; mv nose2-junit.xml test-reports/today/test_testing.xml
 python -m nose2 -s tests test_utils ; mv nose2-junit.xml test-reports/today/test_utils.xml
