6f573f629705f4972044497d36578f27663a53d9
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-02-11 10:07:28 +0100

    updated tests + README.md
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.bat
+++ runtests.bat
@@ -1,22 +1,35 @@
 
 :: use this script to run unit tests locally
 ::
+:: use this script to run unit tests locally
+::
+@echo off
 set CI=True
-python3 runfirst.py
+
+WHERE python3 >nul 2>&1 && (
+    echo Found "python3" command
+    set PYTHON=python3
+) || (
+    echo Did not found "python3" command. use "python" instead
+    set PYTHON=python
+)
+
+start %PYTHON% runfirst.py
 
 mkdir test-reports
 mkdir test-reports\today
-python -m nose2 -s tests test_classificationscheme & move nose2-junit.xml test-reports/today/test_classificationscheme.xml
-python -m nose2 -s tests test_crosshair & move nose2-junit.xml test-reports/today/test_crosshair.xml
-python -m nose2 -s tests test_cursorlocationsvalues & move nose2-junit.xml test-reports/today/test_cursorlocationsvalues.xml
-python -m nose2 -s tests test_init & move nose2-junit.xml test-reports/today/test_init.xml
-python -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/today/test_layerproperties.xml
-python -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/today/test_maptools.xml
-python -m nose2 -s tests test_models & move nose2-junit.xml test-reports/today/test_models.xml
-python -m nose2 -s tests test_plotstyling & move nose2-junit.xml test-reports/today/test_plotstyling.xml
-python -m nose2 -s tests test_qgisissues & move nose2-junit.xml test-reports/today/test_qgisissues.xml
-python -m nose2 -s tests test_spectrallibraries_core & move nose2-junit.xml test-reports/today/test_spectrallibraries_core.xml
-python -m nose2 -s tests test_spectrallibraries_io & move nose2-junit.xml test-reports/today/test_spectrallibraries_io.xml
-python -m nose2 -s tests test_spectrallibraries_plotting & move nose2-junit.xml test-reports/today/test_spectrallibraries_plotting.xml
-python -m nose2 -s tests test_testing & move nose2-junit.xml test-reports/today/test_testing.xml
-python -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/today/test_utils.xml
+call %PYTHON% -m nose2 -s tests test_classificationscheme & move nose2-junit.xml test-reports/today/test_classificationscheme.xml
+call %PYTHON% -m nose2 -s tests test_crosshair & move nose2-junit.xml test-reports/today/test_crosshair.xml
+call %PYTHON% -m nose2 -s tests test_cursorlocationsvalues & move nose2-junit.xml test-reports/today/test_cursorlocationsvalues.xml
+call %PYTHON% -m nose2 -s tests test_example & move nose2-junit.xml test-reports/today/test_example.xml
+call %PYTHON% -m nose2 -s tests test_init & move nose2-junit.xml test-reports/today/test_init.xml
+call %PYTHON% -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/today/test_layerproperties.xml
+call %PYTHON% -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/today/test_maptools.xml
+call %PYTHON% -m nose2 -s tests test_models & move nose2-junit.xml test-reports/today/test_models.xml
+call %PYTHON% -m nose2 -s tests test_plotstyling & move nose2-junit.xml test-reports/today/test_plotstyling.xml
+call %PYTHON% -m nose2 -s tests test_qgisissues & move nose2-junit.xml test-reports/today/test_qgisissues.xml
+call %PYTHON% -m nose2 -s tests test_spectrallibraries_core & move nose2-junit.xml test-reports/today/test_spectrallibraries_core.xml
+call %PYTHON% -m nose2 -s tests test_spectrallibraries_io & move nose2-junit.xml test-reports/today/test_spectrallibraries_io.xml
+call %PYTHON% -m nose2 -s tests test_spectrallibraries_plotting & move nose2-junit.xml test-reports/today/test_spectrallibraries_plotting.xml
+call %PYTHON% -m nose2 -s tests test_testing & move nose2-junit.xml test-reports/today/test_testing.xml
+call %PYTHON% -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/today/test_utils.xml
