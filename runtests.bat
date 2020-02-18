90374e49d064f98aa568cac0de7d797287d66e5d
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-02-18 15:07:08 +0100

    added qpsresourcebrowser.ui
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.bat
+++ runtests.bat
@@ -18,18 +18,20 @@ start %PYTHON% runfirst.py
 
 mkdir test-reports
 mkdir test-reports\today
-call %PYTHON% -m nose2 -s tests test_classificationscheme & move nose2-junit.xml test-reports/today/test_classificationscheme.xml
-call %PYTHON% -m nose2 -s tests test_crosshair & move nose2-junit.xml test-reports/today/test_crosshair.xml
-call %PYTHON% -m nose2 -s tests test_cursorlocationsvalues & move nose2-junit.xml test-reports/today/test_cursorlocationsvalues.xml
-call %PYTHON% -m nose2 -s tests test_example & move nose2-junit.xml test-reports/today/test_example.xml
-call %PYTHON% -m nose2 -s tests test_init & move nose2-junit.xml test-reports/today/test_init.xml
-call %PYTHON% -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/today/test_layerproperties.xml
-call %PYTHON% -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/today/test_maptools.xml
-call %PYTHON% -m nose2 -s tests test_models & move nose2-junit.xml test-reports/today/test_models.xml
-call %PYTHON% -m nose2 -s tests test_plotstyling & move nose2-junit.xml test-reports/today/test_plotstyling.xml
-call %PYTHON% -m nose2 -s tests test_qgisissues & move nose2-junit.xml test-reports/today/test_qgisissues.xml
-call %PYTHON% -m nose2 -s tests test_spectrallibraries_core & move nose2-junit.xml test-reports/today/test_spectrallibraries_core.xml
-call %PYTHON% -m nose2 -s tests test_spectrallibraries_io & move nose2-junit.xml test-reports/today/test_spectrallibraries_io.xml
-call %PYTHON% -m nose2 -s tests test_spectrallibraries_plotting & move nose2-junit.xml test-reports/today/test_spectrallibraries_plotting.xml
-call %PYTHON% -m nose2 -s tests test_testing & move nose2-junit.xml test-reports/today/test_testing.xml
-call %PYTHON% -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/today/test_utils.xml
+%PYTHON% -m coverage run   tests/test_classificationscheme.py
+%PYTHON% -m coverage run --append  tests/test_crosshair.py
+%PYTHON% -m coverage run --append  tests/test_cursorlocationsvalues.py
+%PYTHON% -m coverage run --append  tests/test_example.py
+%PYTHON% -m coverage run --append  tests/test_init.py
+%PYTHON% -m coverage run --append  tests/test_layerproperties.py
+%PYTHON% -m coverage run --append  tests/test_maptools.py
+%PYTHON% -m coverage run --append  tests/test_models.py
+%PYTHON% -m coverage run --append  tests/test_plotstyling.py
+%PYTHON% -m coverage run --append  tests/test_qgisissues.py
+%PYTHON% -m coverage run --append  tests/test_resources.py
+%PYTHON% -m coverage run --append  tests/test_speclib_core.py
+%PYTHON% -m coverage run --append  tests/test_speclib_gui.py
+%PYTHON% -m coverage run --append  tests/test_speclib_io.py
+%PYTHON% -m coverage run --append  tests/test_testing.py
+%PYTHON% -m coverage run --append  tests/test_utils.py
+call %PYTHON% -m coverage report --omit qps/externals/*
