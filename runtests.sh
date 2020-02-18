d34f97aaea50dca23f98e28f8025e3fc715311f6
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-02-18 17:36:07 +0100

    CI
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.sh
+++ runtests.sh
@@ -10,20 +10,20 @@ python3 runfirst.py
 
 mkdir test-reports
 mkdir test-reports/today
-python3 -m tests/test_classificationscheme.py
-python3 -m tests/test_crosshair.py
-python3 -m tests/test_cursorlocationsvalues.py
-python3 -m tests/test_example.py
-python3 -m tests/test_init.py
-python3 -m tests/test_layerproperties.py
-python3 -m tests/test_maptools.py
-python3 -m tests/test_models.py
-python3 -m tests/test_plotstyling.py
-python3 -m tests/test_qgisissues.py
-python3 -m tests/test_resources.py
-python3 -m tests/test_speclib_core.py
-python3 -m tests/test_speclib_gui.py
-python3 -m tests/test_speclib_io.py
-python3 -m tests/test_testing.py
-python3 -m tests/test_utils.py
+python3 tests/test_classificationscheme.py
+python3 tests/test_crosshair.py
+python3 tests/test_cursorlocationsvalues.py
+python3 tests/test_example.py
+python3 tests/test_init.py
+python3 tests/test_layerproperties.py
+python3 tests/test_maptools.py
+python3 tests/test_models.py
+python3 tests/test_plotstyling.py
+python3 tests/test_qgisissues.py
+python3 tests/test_resources.py
+python3 tests/test_speclib_core.py
+python3 tests/test_speclib_gui.py
+python3 tests/test_speclib_io.py
+python3 tests/test_testing.py
+python3 tests/test_utils.py
 python3 -m coverage report --omit qps/externals/*
