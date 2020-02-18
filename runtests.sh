84d55eb6029910e8039b18275eb5060d0e234773
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-02-18 17:11:27 +0100

    CI
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.sh
+++ runtests.sh
@@ -10,20 +10,20 @@ python3 runfirst.py
 
 mkdir test-reports
 mkdir test-reports/today
-python3 -m coverage run   tests/test_classificationscheme.py
-python3 -m coverage run --append  tests/test_crosshair.py
-python3 -m coverage run --append  tests/test_cursorlocationsvalues.py
-python3 -m coverage run --append  tests/test_example.py
-python3 -m coverage run --append  tests/test_init.py
-python3 -m coverage run --append  tests/test_layerproperties.py
-python3 -m coverage run --append  tests/test_maptools.py
-python3 -m coverage run --append  tests/test_models.py
-python3 -m coverage run --append  tests/test_plotstyling.py
-python3 -m coverage run --append  tests/test_qgisissues.py
-python3 -m coverage run --append  tests/test_resources.py
-python3 -m coverage run --append  tests/test_speclib_core.py
-python3 -m coverage run --append  tests/test_speclib_gui.py
-python3 -m coverage run --append  tests/test_speclib_io.py
-python3 -m coverage run --append  tests/test_testing.py
-python3 -m coverage run --append  tests/test_utils.py
+python3 -m coverage run --source qps    tests/test_classificationscheme.py
+python3 -m coverage run --source qps  --append  tests/test_crosshair.py
+python3 -m coverage run --source qps  --append  tests/test_cursorlocationsvalues.py
+python3 -m coverage run --source qps  --append  tests/test_example.py
+python3 -m coverage run --source qps  --append  tests/test_init.py
+python3 -m coverage run --source qps  --append  tests/test_layerproperties.py
+python3 -m coverage run --source qps  --append  tests/test_maptools.py
+python3 -m coverage run --source qps  --append  tests/test_models.py
+python3 -m coverage run --source qps  --append  tests/test_plotstyling.py
+python3 -m coverage run --source qps  --append  tests/test_qgisissues.py
+python3 -m coverage run --source qps  --append  tests/test_resources.py
+python3 -m coverage run --source qps  --append  tests/test_speclib_core.py
+python3 -m coverage run --source qps  --append  tests/test_speclib_gui.py
+python3 -m coverage run --source qps  --append  tests/test_speclib_io.py
+python3 -m coverage run --source qps  --append  tests/test_testing.py
+python3 -m coverage run --source qps  --append  tests/test_utils.py
 python3 -m coverage report --omit qps/externals/*
