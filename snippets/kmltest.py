2b805a26ce896c503f53e2f4069c2623c33df068
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2020-08-20 09:00:03 +0200

    externals/pyqtgraph: upgraded pyqtgraph to 0.11 added kmltest.py
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

---
+++ snippets/kmltest.py
@@ -0,0 +1,19 @@
+from qgis.core import *
+from osgeo import ogr
+from qps.testing import start_app
+start_app()
+p = r'D:\Repositories\qgispluginsupport\test-outputs\speclib2vector\speclib_kml.kml'
+
+lyrName = 'SpectralLibrary'
+lyr = QgsVectorLayer(p + f'|layername={lyrName}')
+print(lyr.fields().names())
+
+ds = ogr.Open(p)
+lyr = ds.GetLayerByName(lyrName)
+
+ldef = lyr.GetLayerDefn()
+fieldNames = [ldef.GetFieldDefn(i).GetName() for i in range(ldef.GetFieldCount())]
+print(fieldNames)
+drv = ds.GetDriver()
+print(drv.GetName())
+s = ""
