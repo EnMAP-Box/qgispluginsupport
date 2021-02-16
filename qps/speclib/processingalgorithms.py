7724296757a12f73a77cf7c64764b1bc172294a6
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2021-02-16 21:16:45 +0100

    added icon to SpectralXUnitConversion
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- qps/speclib/processingalgorithms.py
+++ qps/speclib/processingalgorithms.py
@@ -56,6 +56,9 @@ class SpectralXUnitConversion(QgsProcessingAlgorithm):
         self.mUnitModel = XUnitModel()
         self.mParameters = []
 
+    def icon(self):
+        return QIcon(':/qps/ui/icons/profile.svg')
+
     def name(self):
         return 'spectral_xunit_converter'
 
