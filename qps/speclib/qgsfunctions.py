e5a6d0a1e391eb7a8048dce8b75b79cb3f64dcd5
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2021-03-12 10:46:54 +0100

    modified registration / deregistration of widgets, factories and expression functions
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- qps/speclib/qgsfunctions.py
+++ qps/speclib/qgsfunctions.py
@@ -281,9 +281,18 @@ def registerQgsExpressionFunctions():
             if not QgsExpression.unregisterFunction(func.name()):
                 msgtitle = QCoreApplication.translate("UserExpressions", "User expressions")
                 msg = QCoreApplication.translate("UserExpressions",
-                                                 "The user expression {0} already exists and could not be unregistered.").format(
-                    func.name)
+                                                 "User expression {0} already exists and "
+                                                 "can not be unregistered.").format(func.name)
                 QgsMessageLog.logMessage(msg + "\n", msgtitle, Qgis.Warning)
                 return None
         else:
             QgsExpression.registerFunction(func)
+
+
+def unregisterQgsExpressionFunctions():
+    msgtitle = QCoreApplication.translate("UserExpressions", "User expressions")
+    for name, func in QGIS_FUNCTION_INSTANCES.items():
+        if QgsExpression.unregisterFunction(name):
+            QgsMessageLog.logMessage(f'Unregistered {name}', msgtitle, Qgis.Info)
+        else:
+            QgsMessageLog.logMessage(f'Unable to unregister {name}', msgtitle, Qgis.Warning)
