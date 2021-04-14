10cfee1c2a4830466f51d3a773d1c0bd29805703
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2021-04-14 09:14:57 +0200

    updated to pyqtgraph 0.12.1
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- qps/externals/pyqtgraph/tests/image_testing.py
+++ qps/externals/pyqtgraph/tests/image_testing.py
@@ -44,7 +44,7 @@ Procedure for unit-testing with images:
 # pyqtgraph should be tested against. When adding or changing test images,
 # create and push a new tag and update this variable. To test locally, begin
 # by creating the tag in your ~/.pyqtgraph/test-data repository.
-testDataTag = 'test-data-7'
+testDataTag = 'test-data-8'
 
 
 import time
@@ -137,14 +137,21 @@ def assertImageApproved(image, standardFile, message=None, **kwargs):
         QtGui.QApplication.processEvents()
 
         graphstate = scenegraphState(w, standardFile)
-        image = np.zeros((w.height(), w.width(), 4), dtype=np.ubyte)
-        qimg = fn.makeQImage(image, alpha=True, copy=False, transpose=False)
+        qimg = QtGui.QImage(w.size(), QtGui.QImage.Format.Format_ARGB32)
+        qimg.fill(QtCore.Qt.GlobalColor.transparent)
         painter = QtGui.QPainter(qimg)
         w.render(painter)
         painter.end()
         
-        # transpose BGRA to RGBA
-        image = image[..., [2, 1, 0, 3]]
+        image = fn.imageToArray(qimg, copy=False, transpose=False)
+
+        # the standard images seem to have their Red and Blue swapped
+        if sys.byteorder == 'little':
+            # transpose B,G,R,A to R,G,B,A
+            image = image[..., [2, 1, 0, 3]]
+        else:
+            # transpose A,R,G,B to A,B,G,R
+            image = image[..., [0, 3, 2, 1]]
 
     if message is None:
         code = inspect.currentframe().f_back.f_code
@@ -212,7 +219,7 @@ def assertImageApproved(image, standardFile, message=None, **kwargs):
             else:
                 if os.getenv('TRAVIS') is not None:
                     saveFailedTest(image, stdImage, standardFile, upload=True)
-                elif os.getenv('AZURE') is not None:
+                elif os.getenv('CI') is not None:
                     standardFile = os.path.join(os.getenv("SCREENSHOT_DIR", "screenshots"), standardFile)
                     saveFailedTest(image, stdImage, standardFile)
                 print(graphstate)
@@ -257,7 +264,7 @@ def assertImageMatch(im1, im2, minCorr=None, pxThreshold=50.,
     assert im1.dtype == im2.dtype
 
     if pxCount == -1:
-        if QT_LIB in {'PyQt5', 'PySide2'}:
+        if QT_LIB in {'PyQt5', 'PySide2', 'PySide6', 'PyQt6'}:
             # Qt5 generates slightly different results; relax the tolerance
             # until test images are updated.
             pxCount = int(im1.shape[0] * im1.shape[1] * 0.01)
@@ -469,7 +476,10 @@ def getTestDataRepo():
     """
     global testDataTag
 
-    dataPath = os.path.join(os.path.expanduser('~'), '.pyqtgraph', 'test-data')
+    if os.getenv("CI"):
+        dataPath = os.path.join(os.environ["GITHUB_WORKSPACE"], '.pyqtgraph', 'test-data')
+    else:
+        dataPath = os.path.join(os.path.expanduser('~'), '.pyqtgraph', 'test-data')
     gitPath = 'https://github.com/pyqtgraph/test-data'
     gitbase = gitCmdBase(dataPath)
 
@@ -509,7 +519,7 @@ def getTestDataRepo():
         if not os.path.isdir(parentPath):
             os.makedirs(parentPath)
 
-        if os.getenv('TRAVIS') is not None or os.getenv('AZURE') is not None:
+        if os.getenv('TRAVIS') is not None or os.getenv('CI') is not None:
             # Create a shallow clone of the test-data repository (to avoid
             # downloading more data than is necessary)
             os.makedirs(dataPath)
@@ -600,7 +610,7 @@ def runSubprocess(command, return_code=False, **kwargs):
     if p.returncode != 0:
         print(output)
         err_fun = sp.CalledProcessError.__init__
-        if 'output' in inspect.getargspec(err_fun).args:
+        if 'output' in inspect.getfullargspec(err_fun).args:
             raise sp.CalledProcessError(p.returncode, command, output)
         else:
             raise sp.CalledProcessError(p.returncode, command)
