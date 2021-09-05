662730f11e855e22585602053a8dc7e10d6246f1
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2021-09-05 22:34:09 +0200

    fixed example
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- examples/spectrallibrary.py
+++ examples/spectrallibrary.py
@@ -1,5 +1,5 @@
 from qps.testing import start_app, TestObjects, findQGISResourceFiles
-from qgis.core import QgsRasterLayer
+from qgis.core import QgsRasterLayer, QgsCoordinateReferenceSystem, QgsPointXY
 from qps import initAll
 
 app = start_app(resources=findQGISResourceFiles())
@@ -21,6 +21,7 @@ c.setDestinationCrs(l.crs())
 c.setExtent(l.extent())
 c.show()
 
+
 def loadProfile(crs: QgsCoordinateReferenceSystem, pt: QgsPointXY):
     spatialPoint = SpatialPoint(crs, pt)
     profiles = []
@@ -28,15 +29,16 @@ def loadProfile(crs: QgsCoordinateReferenceSystem, pt: QgsPointXY):
         if isinstance(layer, QgsRasterLayer):
             profile = SpectralProfile.fromRasterLayer(layer, spatialPoint)
             if isinstance(profile, SpectralProfile):
-                profile.setName('Pt {} {}'.format(spatialPoint.x(), spatialPoint.y()))
                 profiles.append(profile)
 
     slw.setCurrentProfiles(profiles)
 
+
 m = CursorLocationMapTool(c)
 m.sigLocationRequest.connect(loadProfile)
 
-slw.sigLoadFromMapRequest.connect(lambda *args : c.setMapTool(m))
+slw.actionSelectProfilesFromMap.setVisible(True)
+slw.sigLoadFromMapRequest.connect(lambda *args: c.setMapTool(m))
 
 # show canvas left to SpectralLibraryWidget
 p = c.pos()
