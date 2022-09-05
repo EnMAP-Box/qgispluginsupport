934bd03c3db8e87e6428d4f6b0dcea2b1d8d0ffe
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2022-09-05 11:07:52 +0200

    modified workflows
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- .docker/run_docker_tests.sh
+++ .docker/run_docker_tests.sh
@@ -21,6 +21,6 @@ export CI=True
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 # python3 -m coverage run -m unittest discover -s tests
 # xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
-python3 scripts/create_runtestsbat.py
+python3 scripts/create_runtests.py
 xvfb-run ./runtests.sh
 popd
