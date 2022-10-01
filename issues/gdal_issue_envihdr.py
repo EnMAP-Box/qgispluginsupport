fad43f22f1b7e3cbbbf67f574f68ab6a7ad1bded
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2022-10-02 01:06:25 +0200

    removed ENVIDataGain and ENVIDataOffset, as they are supported by GDAL 3.6+ added ENVIExampleFile.zip
    Signed-off-by: Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>

--- issues/gdal_issue_envihdr.py
+++ issues/gdal_issue_envihdr.py
@@ -1,11 +1,20 @@
+import pathlib
 import re
 import unittest
 
 from osgeo import gdal
+from qgis.core import QgsRasterLayer
 
 
 class EnviTest(unittest.TestCase):
 
+    def readHeader(self, path_hdr: str):
+        fp = gdal.VSIFOpenL(path_hdr, "rb")
+        hdr: str = gdal.VSIFReadL(1, gdal.VSIStatL(path_hdr).size, fp).decode("utf-8")
+        gdal.VSIFCloseL(fp)
+        return hdr
+
+    @unittest.skipIf(gdal.VersionInfo() < '306', 'Requires GDAL 3.6+')
     def test_EnviDataOffsetAndScale(self):
         path = '/vsimem/test.img'
 
@@ -18,15 +27,9 @@ class EnviTest(unittest.TestCase):
         ds.FlushCache()
         del band, ds
 
-        def readHeader():
-            fp = gdal.VSIFOpenL(path_hdr, "rb")
-            hdr: str = gdal.VSIFReadL(1, gdal.VSIStatL(path_hdr).size, fp).decode("utf-8")
-            gdal.VSIFCloseL(fp)
-            return hdr
-
         # see https://www.l3harrisgeospatial.com/docs/enviheaderfiles.html for definition of
         # ENVI header format
-        hdr = readHeader()
+        hdr = self.readHeader(path_hdr)
         hdr += 'data gain values = {500, 600}\n'
         hdr += 'data offset values = {7, 8}\n'
         fp = gdal.VSIFOpenL(path_hdr, "wb")
@@ -54,7 +57,44 @@ class EnviTest(unittest.TestCase):
         ds.FlushCache()
         del band1, band2, ds
 
-        hdr = readHeader()
+        hdr = self.readHeader(path_hdr)
         # expected behaviour
-        self.assertTrue(re.search('data gain values = {500, 500}', hdr))
-        self.assertTrue(re.search('data offset values = {7, 7}', hdr))
+        self.assertTrue(re.search(r'data gain values = {500, 600}', hdr))
+        self.assertTrue(re.search(r'data offset values = {7, 8}', hdr))
+
+    def test_EnviBandName(self):
+
+        path = '/vsimem/test.img'
+        path = (pathlib.Path(__file__).parent / 'test.img').as_posix()
+
+        drv: gdal.Driver = gdal.GetDriverByName('ENVI')
+        ds: gdal.Dataset = drv.Create(path, 1, 2, 2, eType=gdal.GDT_Byte)
+        path_hdr = [f for f in ds.GetFileList() if f.endswith('.hdr')][0]
+        for b in range(ds.RasterCount):
+            band: gdal.Band = ds.GetRasterBand(b + 1)
+            band.Fill(b + 1)
+            band.GetStatistics(1, 1)
+        ds.FlushCache()
+        del band, ds
+
+        lyr = QgsRasterLayer(path)
+        lyr.reload()
+
+        ds = gdal.Open(path, gdal.GA_Update)
+
+        ds.GetRasterBand(2).SetDescription('Changed Name')
+        ds.FlushCache()
+
+        lyr.reload()
+        hdr = self.readHeader(path_hdr)
+        self.assertTrue('Changed Name' in hdr)
+
+        s = ""
+
+    def test_EnviComments(self):
+
+        pass
+
+    def test_EnviSpectralLibrary(self):
+
+        pass
