5433fa0108f36847ae04208a85cb69d2b0482e0d
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-03-28 23:25:21 +0200

    modified testing

--- .docker/run_docker_tests.sh
+++ .docker/run_docker_tests.sh
@@ -20,7 +20,8 @@ export QT_QPA_PLATFORM=offscreen
 export CI=True
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 # python3 -m coverage run -m unittest discover -s tests
-xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
+# xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
+pytest
 # python3 runfirst.py
 # python3 scripts/create_runtests.py
 # xvfb-run ./runtests.sh
