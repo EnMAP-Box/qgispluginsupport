14622aa2dbedacc6d542cfef5d4556cd05dfcddf
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2023-04-01 00:20:47 +0200

    refactoring

--- scripts/create_resourcefile.py
+++ scripts/create_resourcefile.py
@@ -1,10 +1,14 @@
 import pathlib
 from qgis.testing import start_app
-app = start_app()
+from qps.resources import compileResourceFiles
 
-if __name__ == '__main__':
-    from qps.resources import compileResourceFiles
 
+def create_resource_files():
     QPS_DIR = pathlib.Path(__file__).resolve().parents[1] / 'qps'
     assert QPS_DIR.is_dir()
     compileResourceFiles(QPS_DIR)
+
+
+if __name__ == '__main__':
+    app = start_app()
+    create_resource_files()
