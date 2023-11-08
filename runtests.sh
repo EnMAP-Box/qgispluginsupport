4090b6eda6a04de692b7ae58a88d32c7b0b9bb75
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2023-11-08 15:07:53 +0100

    added pyqis python path to runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export QT_QPA_PLATFORM=offscreen
 export CI=True
+export PYTHONPATH="${PYTHONPATH}"\
+":$(pwd)"\
+":/usr/share/qgis/python/plugins"
+
 rm -Rf test-outputs
 rm -Rf test-reports
 pytest --no-cov-on-fail --cov-config=.coveragec "$@"
