e975f04892975a47153770494774f1bed93599a3
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2024-06-07 10:47:10 +0200

    urlcheck
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- check_urls.sh
+++ check_urls.sh
@@ -5,5 +5,5 @@ urlchecker check \
     --file-types .rst,.md,.py \
     --save test-reports/url-check.csv \
     --exclude-files qps/pyqtgraph \
-    --exclude-urls http://mrcc.com/qgis.dtd \
+    --exclude-urls http://mrcc.com/qgis.dtd,https://bugreports.qt-project.org/browse/QTBUG-18616 \
     qps
