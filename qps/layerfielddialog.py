8cb8b232326077b58966dac2b08f9c330c90d28b
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2025-07-22 13:35:56 +0200

    LayerFieldDialog can hide the field widget, e.g. to select raster layers only

--- qps/layerfielddialog.py
+++ qps/layerfielddialog.py
@@ -1,11 +1,12 @@
 from typing import Callable, Dict, Union, Optional
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QComboBox, QGridLayout, QLabel, QDialogButtonBox, QHBoxLayout, QCheckBox
-from qgis._core import QgsMapLayer, QgsField, QgsFieldProxyModel, QgsMapLayerProxyModel, QgsFieldModel, \
-    QgsMapLayerModel, QgsVectorLayer, QgsFields
+from PyQt5.QtWidgets import QSizePolicy
 
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QComboBox, QGridLayout, QLabel, QDialogButtonBox, QHBoxLayout, QCheckBox
 from qgis.PyQt.QtWidgets import QDialog
+from qgis.core import QgsMapLayer, QgsField, QgsFieldProxyModel, QgsMapLayerProxyModel, QgsFieldModel, \
+    QgsMapLayerModel, QgsVectorLayer, QgsFields
 from qgis.core import QgsProject
 
 
@@ -94,29 +95,42 @@ class SelectLayerFieldDialog(QDialog):
         super().__init__(*args, **kwds)
 
         self.setWindowTitle("Select Layer Field")
-
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
         self.mLayerModel = FilteredMapLayerProxyModel()
         self.mFieldModel = FilteredFieldProxyModel()
 
         self.mLastFields: Dict[str, str] = dict()
 
         self.mLayerComboBox = QComboBox()
+        self.mLayerComboBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
         self.mLayerComboBox.setModel(self.mLayerModel)
         self.mLayerComboBox.currentIndexChanged.connect(self.onLayerChanged)
 
+        self.mShowFieldFilter: bool = True
+        self.mLabelField = QLabel('Field')
         self.mFieldComboBox = QComboBox()
         self.mFieldComboBox.setModel(self.mFieldModel)
+        self.mFieldComboBox.currentIndexChanged.connect(self.validate)
 
-        self.mCbShowAll = QCheckBox(r'Show all layers \& fields')
+        self.mCbShowAll = QCheckBox(r'Show all')
+        self.mCbShowAll.setToolTip(r'Show also layers and fields that did not pass the filter')
         self.mCbShowAll.setChecked(True)
         self.mCbShowAll.toggled.connect(self.setShowAll)
+
         self.mButtonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        btOk = self.mButtonBox.button(QDialogButtonBox.Ok)
+        btOk.clicked.connect(self.accept)
+
+        btCancel = self.mButtonBox.button(QDialogButtonBox.Cancel)
+        btCancel.clicked.connect(self.reject)
 
         layout = QGridLayout()
         layout.addWidget(QLabel('Layer'), 0, 0)
         layout.addWidget(self.mLayerComboBox, 0, 1)
 
-        layout.addWidget(QLabel('Field'), 1, 0)
+        layout.addWidget(self.mLabelField, 1, 0)
         layout.addWidget(self.mFieldComboBox, 1, 1)
 
         layout.addWidget(self.mCbShowAll, 2, 0, 1, 2)
@@ -125,6 +139,14 @@ class SelectLayerFieldDialog(QDialog):
         hbox.addWidget(self.mButtonBox)
         layout.addLayout(hbox, 3, 0, 1, 2)
         self.setLayout(layout)
+
+    def setShowFieldFilter(self, show: bool):
+        self.mShowFieldFilter = show
+        self.mFieldComboBox.setVisible(show)
+        self.mLabelField.setVisible(show)
+
+    def showFieldFilter(self) -> bool:
+        return self.mShowFieldFilter
 
     def setLayer(self, layer: Union[QgsMapLayer, str]) -> bool:
         # save current field selection
@@ -142,6 +164,7 @@ class SelectLayerFieldDialog(QDialog):
                 self.setField(last_field)
                 return True
 
+        self.validate()
         return False
 
     def onLayerChanged(self, index: int):
@@ -162,6 +185,8 @@ class SelectLayerFieldDialog(QDialog):
         if isinstance(lastField, str):
             self.setField(lastField)
 
+        self.validate()
+
     def setShowAll(self, show: bool):
         self.mLayerModel.setShowAll(show)
         self.mFieldModel.setShowAll(show)
@@ -170,8 +195,10 @@ class SelectLayerFieldDialog(QDialog):
         return self.mLayerComboBox.currentData(QgsMapLayerModel.CustomRole.Layer)
 
     def field(self) -> Optional[str]:
-
-        return self.mFieldComboBox.currentData(QgsFieldModel.CustomRole.FieldName)
+        if self.showFieldFilter():
+            return self.mFieldComboBox.currentData(QgsFieldModel.CustomRole.FieldName)
+        else:
+            return None
 
     def setField(self, field: Union[QgsField, str]) -> bool:
 
@@ -183,17 +210,33 @@ class SelectLayerFieldDialog(QDialog):
             if fname == field:
                 self.mFieldComboBox.setCurrentIndex(i)
                 return True
-        return False
 
-    def isValid(self):
         return False
 
     def setProject(self, project):
         assert isinstance(project, QgsProject)
         self.mLayerModel.setProject(project)
 
-    def setLayerFilter(self, func: Callable):
+    def setLayerFilter(self, func: Optional[Callable]):
         self.mLayerModel.setFilterFunc(func)
 
-    def setFieldFilter(self, func: Callable):
-        self.mFieldModel.setFilterFunc(func)
+    def setFieldFilter(self, func: Optional[Callable]):
+
+        if callable(func):
+            self.mFieldModel.setFilterFunc(func)
+            self.setShowFieldFilter(True)
+
+        else:
+            self.setShowFieldFilter(False)
+
+    def validate(self) -> bool:
+
+        lyr = self.layer()
+        b = isinstance(lyr, QgsMapLayer)
+
+        if self.mFieldComboBox.isVisible():
+            b &= isinstance(lyr, QgsVectorLayer) and self.field() in lyr.fields().names()
+
+        self.mButtonBox.button(QDialogButtonBox.Ok).setEnabled(b)
+
+        return b
