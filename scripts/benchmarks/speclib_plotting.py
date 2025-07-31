b87c2da6367ab9e257520904ffe3f2fba79c10e1
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2025-07-31 11:50:24 +0200

    better printouts for plotting benchmark

--- scripts/benchmarks/speclib_plotting.py
+++ scripts/benchmarks/speclib_plotting.py
@@ -1,9 +1,23 @@
 import datetime
+import logging
+import os
+from typing import Optional
 
+os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5'
 from qgis.core import QgsVectorLayer
 from qps import initAll
+from qps.speclib.gui.spectrallibraryplotitems import SpectralProfilePlotDataItem
+from qps.speclib.gui.spectrallibraryplotwidget import SpectralProfilePlotModel
+import qps.speclib.gui.spectrallibraryplotwidget
 from qps.speclib.gui.spectrallibrarywidget import SpectralLibraryWidget
 from qps.testing import start_app
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(module)s %(funcName)s: %(message)s',
+                    handlers=[
+                        # logging.FileHandler("debug.log"),  # Log to a file
+                        logging.StreamHandler()  # Log to console
+                    ])
 
 app = start_app()
 initAll()
@@ -11,22 +25,45 @@ initAll()
 path_large_speclib = r'D:\Repositories\qgispluginsupport\tmp\largespeclib\speclib_namibia2024.gpkg'
 
 t0 = datetime.datetime.now()
+TIMES = dict()
+qps.speclib.gui.spectrallibraryplotwidget.MAX_PROFILES_DEFAULT = 10000
+
+
+def measure(step: str, end: Optional[str] = None):
+    global t0
+    dt = datetime.datetime.now() - t0
+    TIMES[step] = dt
+    print(f'{dt}: {step}', end=end)
+    t0 = datetime.datetime.now()
+
+
+def n_items(w: SpectralLibraryWidget):
+    items = [item for item in w.plotWidget().getPlotItem().items if isinstance(item, SpectralProfilePlotDataItem)]
+    return len(items)
+
+
+t0 = datetime.datetime.now()
 layer = QgsVectorLayer(path_large_speclib)
 assert layer.isValid()
-t1 = datetime.datetime.now()
+# layer.setSubsetString('"fid" <= 550')
 
-w = SpectralLibraryWidget(speclib=layer)
+measure(f'Open Layer with {layer.featureCount()} features')
+
+w = SpectralLibraryWidget(speclib=layer, profile_fields_check=False)
+model: SpectralProfilePlotModel = w.plotModel() if hasattr(w, 'plotModel') else w.plotControl()
 w.show()
 app.processEvents()
-if hasattr(w, 'plotModel'):
-    model = w.plotModel()
-    model.setMaxProfiles(999999)
+measure(f'Open Widget ({n_items(w)} profiles, max: {model.maxProfiles()})')
+
+t0 = datetime.datetime.now()
+model.setMaxProfiles(99999)
+app.processEvents()
+if hasattr(model, 'flushProxySignals'):
     model.flushProxySignals()
 
-t2 = datetime.datetime.now()
+measure(f'Increase maxProfiles to 99999 ({n_items(w)} profiles)')
 
-items = list(model.plotWidget().spectralProfilePlotDataItems())
-print(f'Load and visualize {layer.featureCount()} features with {len(items)} profiles:')
-print(f'Open Layer: {t1 - t0}')
-print(f'Load Widget: {t2 - t1}')
-app.exec_()
+model.updatePlot()
+measure(f'Replot all ({n_items(w)} profiles)')
+
+# app.exec_()
