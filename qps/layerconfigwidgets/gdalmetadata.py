165cfdb0545e0fa202f350b268ca344864d1ae40
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2026-01-14 15:00:10 +0100

    Q5 to 6: QRegExp -> QRegularExpression

--- qps/layerconfigwidgets/gdalmetadata.py
+++ qps/layerconfigwidgets/gdalmetadata.py
@@ -33,8 +33,8 @@ from typing import Any, Dict, List, Match, Pattern, Tuple, Union
 
 from osgeo import gdal, ogr
 
-from qgis.PyQt.QtCore import NULL, QAbstractTableModel, QMimeData, QModelIndex, QRegExp, \
-    QSortFilterProxyModel, Qt, QTimer, QUrl
+import qgis.PyQt.QtCore
+from qgis.PyQt.QtCore import NULL, QAbstractTableModel, QMimeData, QModelIndex, QSortFilterProxyModel, Qt, QTimer, QUrl
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction, QApplication, QCheckBox, QComboBox, QDialog, QDialogButtonBox, QGridLayout, \
     QGroupBox, QHBoxLayout, QLabel, QLineEdit, QMenu, QSizePolicy, QTableView, QWidget
@@ -42,6 +42,12 @@ from qgis.core import edit, Qgis, QgsAttributeTableConfig, QgsDefaultValue, QgsE
     QgsFeatureRequest, QgsField, QgsFieldConstraints, QgsMapLayer, QgsRasterDataProvider, QgsRasterLayer, QgsVectorLayer
 from qgis.gui import QgsAttributeEditorContext, QgsAttributeTableModel, QgsDualView, QgsFieldCalculator, QgsGui, \
     QgsMapCanvas, QgsMapLayerConfigWidgetFactory, QgsMessageBar
+
+if qgis.PyQt.QtCore.QT_VERSION_STR[0] == '5':
+    from qgis.PyQt.QtCore import QRegExp as QRegularExpression
+else:
+    from qgis.PyQt.QtCore import QRegularExpression
+
 from .core import QpsMapLayerConfigWidget
 from .. import debugLog
 from ..classification.classificationscheme import ClassificationScheme, ClassificationSchemeWidget
@@ -195,7 +201,7 @@ def value2str(v: Any) -> str:
         return str(v)
 
 
-def filterFeatures(layer: QgsVectorLayer, regex: QRegExp) -> List[int]:
+def filterFeatures(layer: QgsVectorLayer, regex: QRegularExpression) -> List[int]:
     fids = []
     for f in layer.getFeatures():
         f: QgsFeature
@@ -1652,11 +1658,11 @@ class GDALMetadataModelConfigWidget(QpsMapLayerConfigWidget):
             matchCase = Qt.CaseInsensitive
 
         if optionRegex.isChecked():
-            syntax = QRegExp.RegExp
+            syntax = QRegularExpression.RegExp
         else:
-            syntax = QRegExp.Wildcard
+            syntax = QRegularExpression.Wildcard
 
-        rx = QRegExp(text, cs=matchCase, syntax=syntax)
+        rx = QRegularExpression(text, cs=matchCase, syntax=syntax)
         if isinstance(view, QgsDualView):
             metadataModel = view.masterModel().layer()
             if rx.isValid():
