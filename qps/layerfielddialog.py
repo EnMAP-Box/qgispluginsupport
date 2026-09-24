7cdbe154460ec78a4a61cd3206eb0616c4e82abe
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2026-09-25 00:22:28 +0200

    docstrings FilteredMapLayerProxyModel.setFilterFunc and FilteredFieldProxyModel.setFilterFunc

--- qps/layerfielddialog.py
+++ qps/layerfielddialog.py
@@ -108,6 +108,9 @@ class FilteredFieldProxyModel(QgsFieldProxyModel):
         self.invalidateFilter()
 
     def setFilterFunc(self, func: Callable):
+        """
+        Set a filter function `f(field: QgsField) -> bool` that returns True if a QgsField should be shown
+        """
         self.mFilterFunc = func
         self.invalidateFilter()
 
